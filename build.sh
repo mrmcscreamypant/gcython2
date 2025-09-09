9577e10f9abd158233bc04a4f2051d678994e61f
mrmcscreamypant <161328176+mrmcscreamypant@users.noreply.github.com>
2025-09-09 22:13:01 +0000

    class defs

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-textx generate --target dot --overwrite $1
-textx generate --target dot --grammar $1 --overwrite *.$2
+textx generate --target dot --overwrite $1 -o .
+textx generate --target dot --grammar $1 --overwrite $2 -o .
 mv *.dot renders
 dot -Tpng -O  renders/*.dot
 rm -r renders/*.dot
