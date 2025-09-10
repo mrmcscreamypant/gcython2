35d3a6ef42e1a4ef8efd8b62b2055fd957488eb4
jrs <nil>
2025-09-10 11:54:46 -0400

    remove unused import

--- src/gcython/__main__.py
+++ src/gcython/__main__.py
@@ -4,7 +4,6 @@ print(__doc__)
 from gcython.expressions import Num
 from gcython.expressions.operations import Sqrt
 from pylatexenc.latex2text import latex2text
-from rich.markdown import Markdown
 
 latex = "\\begin{math}"+Sqrt(Num(0.2)).__latex__()+"\\end{math}"
 markdown = latex2text(latex)
